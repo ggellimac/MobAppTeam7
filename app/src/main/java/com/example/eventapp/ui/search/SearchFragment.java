b817a2ab9ed17b3994e6543e2e2aafbbf81fa6f7
Camille Pascua <cpascua78@gmail.com>
2020-04-19 15:38:16 -0400

    fixed up home page and connected all pages to nav bar

--- app/src/main/java/com/example/eventapp/ui/search/SearchFragment.java
+++ app/src/main/java/com/example/eventapp/ui/search/SearchFragment.java
@@ -22,7 +22,7 @@ public class SearchFragment extends Fragment {
         searchViewModel =
                 ViewModelProviders.of(this).get(SearchViewModel.class);
         View root = inflater.inflate(R.layout.fragment_search, container, false);
-        final TextView textView = root.findViewById(R.id.text_search);
+      //  final TextView textView = root.findViewById(R.id.text_search);
         // notificationsViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
         //    @Override
         //      public void onChanged(@Nullable String s) {

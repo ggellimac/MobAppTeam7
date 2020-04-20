cbe74a8193cef6ea010a48a608a9f8b3cb80d203
Oghenero Ekwevugbe <nerotechstation@ogheneros-mbp.attlocal.net>
2020-04-19 21:56:07 -0400

    Profile page

--- app/src/main/java/com/example/eventapp/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/example/eventapp/ui/profile/ProfileFragment.java
@@ -16,20 +16,6 @@ import androidx.lifecycle.ViewModelProviders;
 import com.example.eventapp.R;
 public class ProfileFragment extends Fragment {
 
-    private ProfileViewModel profileViewModel;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        profileViewModel =
-                ViewModelProviders.of(this).get(ProfileViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_profile, container, false);
-        final TextView textView = root.findViewById(R.id.text_profile);
-        // notificationsViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-        //    @Override
-        //      public void onChanged(@Nullable String s) {
-        //          textView.setText(s);
-        //      }
-        //   });
-        return root;
-    }
+
 }
